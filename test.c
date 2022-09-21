bfd16272cdfb9e86878e2c2429b58f3744bd8a54
Andrei <andrei.ceban.032@gmai.com>
2022-09-21 22:14:13 +0300

    last changes

--- test.c
+++ test.c
@@ -2,71 +2,69 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void delete(int);
-
 struct node {
     int data;
     struct node *next;
     struct node *last;
 };
 
-void append(struct node** ref_head, int new_data){
-    struct node *new_node = (struct node*)malloc(sizeof(struct node));
-    struct node *last = *ref_head;
+void append(struct node** head, int data){
+    if((*head) == NULL) {
+        (*head) = (struct node*)malloc(sizeof(struct node));
+        (*head)->data = data;
+    } else {
+        struct node *add = (struct node*)malloc(sizeof(struct node));
+        add->data = data;
 
-    new_node->data = new_data;
-
-    new_node->next = NULL;
-    
-    if(*ref_head == NULL){
-        *ref_head = new_node;
-        return;
+        struct node *tail = *head;
+        while(tail->next != NULL){
+            tail = tail->next;
+        }
+        tail->next = add;
+        add->last = tail;
+        add->next = NULL;
     }
 
-    while(last->next != NULL){
-        last = last->next;
-    }
-
-    last->next = new_node;
 }
 
-void prepend(struct node **head, int val){
-    struct node *new_node = (struct node*)malloc(sizeof(struct node));
-    new_node->data = val;
-    new_node->next = *head;
-    *head = new_node;
+void prepend(struct node **head, int data){
+    if(head == NULL){
+    	(*head) = (struct node*)malloc(sizeof(struct node));
+    	(*head)->data = data;
+    }else {
+    	struct node *add = (struct node*)malloc(sizeof(struct node));
+    	add->data = data;
+    	add->next = (*head);
+    	(*head)->last = add;
+    	(*head) = add;
+    }
 }
 
 void reverse(struct node **head){
-    struct node* prev = NULL;
-    struct node* current = *head;
-    struct node* next = NULL;
-
-    while(current != NULL){
-        next = current->next;
-
-        current->next = prev;
-
-        prev = current;
-        current = next;
-    }
-    *head = prev;
-}
-
-void spec_index(struct node **current, int pos, int val) {
-    if(pos < 1 || pos > (val + 1)) {
-        printf("%s","Wrong Position ! \n");
-    } else {
-        while(pos--){
-            if(pos == 0){
-
-            }
-        }
+    if(head == NULL) {
+    	printf("Adaugati valori pentru a face reversie :)) ");
+    }else {
+    	struct node *tail = *head;
+    	while(tail->next != NULL) {
+    	    tail = tail->next;
+    	}
+    	
+    	struct node *temp = tail;
+    	struct node *trans;
+    	
+    	while(temp != NULL){
+    	    trans = temp->last;
+    	    temp->last = temp->next;
+    	    temp->next = trans;
+    	    temp = temp->next;
+    	}
+    	(*head) = tail;	
     }
 }
 
 
-int getCurrSize(struct node* node){
+// get the size of the head
+int getSize(struct node* node){
     int size=0;
 
     while(node!=NULL){
@@ -76,56 +74,71 @@ int getCurrSize(struct node* node){
     return size;
 }
 
-
 //function to insert after nth node
-void insertAfterNthNode(int n, int data, struct node** head)
+void add_value_to_index(struct node **head,int pos,int data)
 {
-    int size = getCurrSize(*head);
+    int size = getSize(*head);
 
-    struct node* newNode = (struct node*) malloc(sizeof(struct node)); 
-    newNode->data = data;
-    newNode->next = NULL;
-    
-    // Can't insert if position to insert is greater than size of Linked List
-    // can insert after negative pos
-    if(n < 0 || n > size) 
-        printf("Invalid position to insert\n"); 
+    struct node* add = (struct node*)malloc(sizeof(struct node)); 
+    add->data = data;
+    add->next = NULL;
+
+    if(pos < 0 || pos > size) 
+        printf("Pozitie invalida !!!\n"); 
+    else if(pos == 0){
+        add->next = *head; 
+        *head = add;
+    }else {
+        // traversam linked lists
+        struct node *temp = *head; 
         
-    // inserting first node
-    else if(n == 0){
-        newNode->next = *head; 
-        *head = newNode;
-    }
-    
-    else 
-    {
-        // temp used to traverse the Linked List
-        struct node* temp = *head; 
+        while(--pos)
+            temp = temp->next;
         
-        // traverse till the nth node
-        while(--n)
-            temp=temp->next;
-        
-        // assign newNode's next to nth node's next
-        newNode->next= temp->next;
-        // assign nth node's next to this new node
-        temp->next = newNode;
-        // newNode inserted b/w 3rd and 4th node
+        add->next= temp->next;
+        temp->next = add;
     }
 }
 
 
-bool search_for_value(struct node* head, int x)
+void remove_value(struct node **head, int pos)
 {
-    struct node* current = head; // Initialize current
-    while (current != NULL) {
-        if (current->data == x)
-            return true;
-        current = current->next;
+    if(pos <= 0){
+    	printf("Adauga terog o positie valida :)) ");
     }
-    return false;
-}
 
+    struct node* temp = (*head);
+    int value = 1;
+    
+    while(temp != NULL) {
+    	if(value == pos){
+    	    struct node *remove = temp;
+    	    
+    	    if(temp->last != NULL) {
+    	    	temp->last->next = temp->next;
+    	    }
+    	    
+    	    if(temp->next != NULL){
+    	    	temp->next->last = temp->last;
+    	    }
+    	    
+    	    if(value == 1 && temp->next == NULL) {
+    	    	*head = NULL;
+    	    }
+    	    
+    	    free(remove);
+    	    break;
+    	}else {
+    	    value++;
+    	    temp = temp->next;
+    	}
+    }
+    
+    if(value < pos) {
+    	printf("Nu exista asa elemente :))");
+    }
+    
+}
 
 void sortedInsert(struct node** head, struct node* newNode)
 {
@@ -141,74 +154,33 @@ void sortedInsert(struct node** head, struct node* newNode)
     current->next = newNode;
     *head = dummy.next;
 }
-// Given a list, change it to be in sorted order (using `sortedInsert()`).
+
 void insertSort(struct node **head)
 {
-    struct node* result = NULL;     // build the answer here
-    struct node* current = *head;   // iterate over the original list
+    struct node* result = NULL;     
+    struct node* current = *head;  
     struct node* next;
  
-    while (current != NULL)
-    {
-        // tricky: note the next pointer before we change it
+    while (current != NULL){
         next = current->next;
- 
         sortedInsert(&result, current);
         current = next;
     }
  
-    *head = result;
+    (*head) = result;
 }
 
-void show_ls(struct node *head){
-    struct node *temp = head;
 
-    while(temp != NULL) {
-        printf(" %d ", temp->data);
+bool search_for_value(struct node* head, int value)
+{
+    struct node* temp = head; 
+    while (temp != NULL) {
+        if (temp->data == value)
+            return true;
         temp = temp->next;
     }
+    return false;
 }
-
-void deletes(struct node *head,int pos)
-{
-    struct node* temp = head; // Creating a temporary
-                              // variable pointing to head
-    int i;
-    if (pos == 0) {
-        printf("\nElement deleted is : %d\n", temp->data);
-        head = head->next; // Advancing the head pointer
-        temp->next = NULL;
-        free(temp); // Node is deleted
-    }
-    else {
-        for (i = 0; i < pos - 1; i++) {
-            temp = temp->next;
-        }
-        // Now temp pointer points to the previous node of
-        // the node to be deleted
-        struct node* del = temp->next; // del pointer points to the node
-                          // to be deleted
-        temp->next = temp->next->next;
-        printf("\nElement deleted is : %d\n", del->data);
-        del->next = NULL;
-        free(del); // Node is deleted
-    }
-    printf("\nUpdated Linked List is : \n");
-
-    return;
-}
-
-int findlength(struct node* head)
-{
-    struct node* curr = head;
-    int cnt = 0;
-    while (curr != NULL) {
-        cnt++;
-        curr = curr->next;
-    }
-    return cnt;
-}
-
 
 void combineTwoLists(struct node** head, struct node** head2) {
     struct node* val = *head2;
@@ -218,44 +190,41 @@ void combineTwoLists(struct node** head, struct node** head2) {
     }
 }
 
-void Backwards(struct node** head) {
+void backwards_traversal(struct node **head) {
     struct node *tail = *head;
+    
     while (tail->next != NULL)
         tail = tail->next;
-    struct node* ptr = tail;
+    struct node* temp = tail;
 
-    while(ptr != NULL) {
-        printf("%d ", ptr->data);
-        ptr = ptr->last;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->last;
     }
     printf("\n");
 }
 
 
-// Function to convert the
-// Linked List to an array
-void convertArr(struct node* head,int arr[],int* index)
-{
-    
+void show_ls(struct node *head){
+    struct node *temp = head;
 
-    // Traverse the Linked List and add the
-    // elements to the array one by one
-    while (head != NULL) {
-        arr[*index++] = head->data;
-        head = head->next;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
     }
+    printf("\n");
 }
+
 
 int main() {
     int number = 1;
     int element;
     int index;  
-    struct node *head = NULL; // store the addres of the first node
-    struct node *head2 = NULL; // store the addres of the first node
-    FILE *fp;
+    struct node *head = NULL;
+    struct node *head2 = NULL;
     int len;
     
-    while(number != 12) {
+    while(number != 15) {
         printf("- 1 Append \n");
         printf("- 2 Prepend \n");
         printf("- 3 Reverse \n");
@@ -263,11 +232,12 @@ int main() {
         printf("- 5 Remove a value at index \n");
         printf("- 6 Sort the list \n");
         printf("- 7 Search for a value \n");
-        printf("- 8 Join two linked lists \n");
-        printf("- 9 Backwards traversal \n");
-        printf("- 10 Load the data structure \n");
-        printf("- 11 Type the data structure and save it in a file \n");
-        printf("- 12 Exit \n");
+        printf("- 8 Add elements for second list to combine the lists : \n");
+        printf("- 9 Join two linked lists \n");
+        printf("- 10 Backwards traversal \n");
+        printf("- 11 Load the data structure \n");
+        printf("- 13 Save data in the file \n");
+        printf("\n");
         
         printf("Type a number to execute a command : ");
 
@@ -281,8 +251,8 @@ int main() {
         case 1:
             printf("\n Append and element :");
             scanf("%d", &element);
+            printf("\n");
             append(&head, element);
-            append(&head2, element);
             printf("The list is : ");
             show_ls(head);
             printf("\n");
@@ -290,6 +260,7 @@ int main() {
         case 2:
             printf("\n Prepend and element :");
             scanf("%d", &element);
+            printf("\n");
             prepend(&head, element);
             printf("The list is : ");
             show_ls(head);
@@ -297,6 +268,7 @@ int main() {
             break;
         case 3:
             printf("\n Reverse the list :");
+            printf("\n");
             reverse(&head);
             printf("The reversed list is : ");
             show_ls(head);
@@ -305,17 +277,19 @@ int main() {
         case 4:
             printf("\n Add a value : ");
             scanf("%d", &element);
+            printf("\n");
             printf("\n Add a index : ");
             scanf("%d", &index);
-            insertAfterNthNode(index,element, &head);
+            printf("\n");
+            add_value_to_index(&head,index,element);
             printf("The list is : ");
             show_ls(head);
             printf("\n");
             break;
         case 5:
-            printf("\n Enter the index of the numebr :");
+            printf("\n Enter the index of number to remove :");
 	    scanf("%d", &element);
-            deletes(head, element);
+            remove_value(&head, element);
             show_ls(head);
             printf("\n");
             break;
@@ -328,52 +302,60 @@ int main() {
         case 7:
             printf("\n What number are you looking for :");
             scanf("%d", &element);
+            printf("\n");
 	    printf("\n");
             search_for_value(head,element) ? printf("Exist \n") : printf("Not exist ! \n");
             printf("\n");
             break;
         case 8:
+            printf("\n Add and element for second list :");
+            scanf("%d", &element);
+            printf("\n");
+            append(&head2, element);
+            printf("The list is : ");
+            show_ls(head2);
+            printf("\n");
+            break;
+        case 9:
             printf("The combined lists : ");
             combineTwoLists(&head,&head2);
             show_ls(head);
             printf("\n");
             break;
-        case 9:
+        case 10:
             printf("Backwards traversal: ");
-            Backwards(&head);
+            backwards_traversal(&head);
             show_ls(head);
             printf("\n");
             break;
-        case 10:
-            fp=fopen("elements.txt","r");
-            int* list=malloc(100*sizeof(*list));
-            int j=0,n;
-            while(fscanf(fp,"%d",&n)>0){
-                list[j]=n;
-                j++;
-            }
-            for(int i=0;i<j;i++){
-                append(&head,list[i]);
-            }
+        case 11:
+            FILE* fp;
+	    fp = fopen("elements.txt", "r");
+
+	    int fuck = 0;
+	    while(!feof(fp)) {
+		fscanf(fp,"%d ",&element);
+		append(&head, element);
+	    }
+
             fclose(fp);
 
             printf("The list is : ");
             show_ls(head);
             printf("\n");
             break;
-        case 11:
-            len = findlength(head);
-            int* randbl=malloc(len*sizeof(*randbl));
-            int i=0;
-            convertArr(head,randbl,&i);
-            for(int i=0;i<len;i++){
-                printf("%d ",randbl[i]);
-            };
-            break;
-        case 12:
+	case 13:
+    	    fp = fopen("elements.txt", "w");
+    	    struct node* ptr = head;
+    	    while(ptr != NULL) {
+            	fprintf(fp, "%d ", ptr->data);
+             	ptr = ptr->next;
+    	    }	    
+    	    fclose(fp);
             break;
         default:
             printf("Error! Invalid choice.");
+            printf("\n");
         }
     }
 
